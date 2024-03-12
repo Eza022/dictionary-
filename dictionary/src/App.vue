eb274ab6b3df822f05e74c93b1918856f9233365
Eza022 <sithukuzaphumeza@gmail.com>
2024-03-12 20:39:53 +0200

    start

---
+++ dictionary/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <form @submit.prevent="getDefinitions()">
+    <input placeholder="myWord" type = 'text'id="word"/> 
+    <button type="submit"> Get definition</button>
+  </form>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+ 
+  }
+
+</script>
+
+<style>
+
+
+</style>
